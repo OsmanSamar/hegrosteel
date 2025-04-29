9943da6c2353ba81f5d2e1509f6eb362230484e2
Samar <osmansamar367@gmail.com>
2025-04-29 11:19:46 +0200

    Added fonts and style

--- front-page.php
+++ front-page.php
@@ -1,12 +1,13 @@
-<?php get_header() ?>
+<?php get_header()
+//Template Name: frontpage 
+
+?>
 
 <main class="front-page">
     <div class="container">
-        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
+       
 
-            <?php the_content(); ?>
-        <?php endwhile; endif; ?>
+    
     </div>
 </main>
 
