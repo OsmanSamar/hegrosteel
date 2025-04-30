b5f1371dd42f03e756be1021c2f075917c5b9c88
Samar <osmansamar367@gmail.com>
2025-04-30 12:32:11 +0200

    Updated style of quto-section

--- components/call_to_action_section.php
+++ components/call_to_action_section.php
@@ -4,8 +4,9 @@ $fields = $args['fields'];
 <div class="call_to_action_section">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-6 col-lg-5 col-xl-8 d-flex flex-column justify-content-start align-items-start mb-5" data-aos="fade-up"
-                data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out">
+            <div class="col-12 col-md-6 col-lg-5 col-xl-8 d-flex flex-column justify-content-start align-items-start mb-5"
+                data-aos="fade-up" data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000"
+                data-aos-easing="ease-in-out">
                 <div
                     class="d-flex flex-column flex-lg-row justify-content-between align-items-start align-items-lg-center">
                     <div class="content">
@@ -17,7 +18,8 @@ $fields = $args['fields'];
 
                 </div>
             </div>
-            <div class=" col-md-6 col-lg-3 offset-lg-1 col-xl-3 offset-xl-1 d-flex align-items-center justify-content-center">
+            <div
+                class=" col-md-6 col-lg-3 offset-lg-1 col-xl-3 offset-xl-1 d-flex align-items-center justify-content-center">
                 <?php if (!empty($fields['button'])): ?>
                     <a href="<?= esc_url($fields['button']['url']); ?>"
                         target="<?= esc_attr($fields['button']['target']); ?>" class="button custom-button">
@@ -29,3 +31,4 @@ $fields = $args['fields'];
             </div>
         </div>
     </div>
+</div>
--- components/quote_section.php
+++ components/quote_section.php
@@ -4,7 +4,7 @@ $fields = $args['fields'];
 <div class="quote_section">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-12 col-lg-10 offset-lg-2 col-xl-8 offset-xl-4 d-flex flex-column justify-content-start align-items-start mb-5"
+            <div class="col-12 col-md-12 col-lg-10 offset-lg-2 col-xl-8 offset-xl-2 d-flex flex-column justify-content-start align-items-start mb-5"
                 data-aos="fade-up" data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000"
                 data-aos-easing="ease-in-out">
                 <div class="d-flex flex-column flex-lg-row  align-items-center ">
