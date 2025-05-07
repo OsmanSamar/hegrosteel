bca49ab73dffbedb148b475260b60f2dbd719a7d
Samar <osmansamar367@gmail.com>
2025-05-07 09:23:44 +0200

    Updated footer style

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@ $footer_menu = array_chunk($items, ceil(count($items) / 2));
 ?>
 
 <footer class="footer">
-    <div class="container">
+    <div class="">
         <div class="row">
             <div class="col-lg-10">
                 <div class="d-flex justify-content-between gap-3 flex-wrap">
@@ -25,7 +25,7 @@ $footer_menu = array_chunk($items, ceil(count($items) / 2));
 
         <div class="row">
 
-            <div class="col-lg-3 ">
+            <div class="col-lg-3 col-md-3 ">
                 <a href="<?= esc_url(get_permalink(get_page_by_path('front-page'))) ?>">
                     <img src="<?= get_template_directory_uri() ?>/images/logo.svg" alt="Hegrosteel Logo"
                         class="footer-logo mb-3" />
@@ -42,7 +42,7 @@ $footer_menu = array_chunk($items, ceil(count($items) / 2));
                 </div>
             </div>
 
-            <div class="col-lg-8 offset-lg-1">
+            <div class="col-lg-8 offset-lg-1 col-md-8 offset-md-1">
                 <div class="row">
 
                     <div class="col-md-3 mb-3">
