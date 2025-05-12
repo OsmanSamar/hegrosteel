b9032caa2326c1dde1618b52c52f6e556ac67a4f
Samar <osmansamar367@gmail.com>
2025-05-12 17:08:02 +0200

    Updated tabs met Frank, Updated form style

--- components/tabs.php
+++ components/tabs.php
@@ -1,24 +1,6 @@
 <?php
 $fields = $args['fields'];
 ?>
-<!-- <div class="row">
-    <div class="col-lg-12">
-        <div class="tabs tab-right">
-            <nav class="border-wrap">
-               <a href="#tab1" title="tab1" class="tab-link active border-wrap" data-tab="content1">
-               <h4><?= '' // $fields['tab_title']; ?></h4>
-            </a>
-                <a href="#tab2 border-wrap" class="tab-link" data-tab="content2">Tab2</a>
-                <a href="#tab3 border-wrap" class="tab-link" data-tab="content3">Tab3</a>
-            </nav>
-
-            <div class="tab-content" id="content1">Content 1 goes here.</div>
-            <div class="tab-content" id="content2" style="display: none;">Content 2 goes here.</div>
-            <div class="tab-content" id="content3" style="display: none;">Content 3 goes here.</div>
-        </div>
-    </div>
-</div> -->
-
 <?php if ($fields['tab']): ?>
     <div class="row">
         <div class="col-lg-12">
@@ -52,7 +34,7 @@ $fields = $args['fields'];
                                 <div class="col-lg-6">
 
                                 </div>
-                                
+
                             </div>
 
                                 <h3><?= $tab['title']; ?></h3>
