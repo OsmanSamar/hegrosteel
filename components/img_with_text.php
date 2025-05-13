55acdce59d8e0d9be80f4d38d500b21b86d5bafe
Samar <osmansamar367@gmail.com>
2025-05-13 11:55:15 +0200

    Updated style

--- components/img_with_text.php
+++ components/img_with_text.php
@@ -10,13 +10,13 @@ $backgroundClass= $fields['background'] ?? '';
     <div class="container">
         <div class="row d-flex flex-wrap">
             <!-- Image Column -->
-            <div class="col-12 col-md-6 col-lg-6 d-flex flex-column mb-5 <?= $imageOnLeft ? 'order-1' : 'order-2'; ?>"
+            <div class="col-12 col-md-12 col-lg-6 d-flex flex-column mb-5 mb-lg-0 <?= $imageOnLeft ? 'order-1' : 'order-2'; ?>"
                 data-aos="fade-up" data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out">
                 <img src="<?= esc_url($fields['image']['url']); ?>" alt="<?= esc_html($fields['image']['alt']); ?>" class="img-text" />
             </div>
 
             <!-- Content Column -->
-            <div class="col-md-6 col-lg-5 offset-lg-1 d-flex align-items-center justify-content-center mb-5 <?= $imageOnLeft ? 'order-2' : 'order-1'; ?>">
+            <div class="col-12 col-md-12 col-lg-5 offset-lg-1 d-flex align-items-center justify-content-center mb-5 <?= $imageOnLeft ? 'order-2' : 'order-1'; ?>">
                 <div class="d-flex flex-column flex-lg-row justify-content-between align-items-start align-items-lg-center">
                     <div class="content">
                         <h3><?= $fields['title']; ?></h3>
