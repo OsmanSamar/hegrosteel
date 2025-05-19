4baa785df6fc3e289666c015fab61b7768632ac7
Samar <osmansamar367@gmail.com>
2025-05-19 15:00:10 +0200

    Updated According feedback

--- components/call_to_action_section.php
+++ components/call_to_action_section.php
@@ -4,22 +4,18 @@ $fields = $args['fields'];
 <div class="call_to_action_section">
     <div class="">
         <div class="row">
-            <div class="col-12 col-md-6 col-lg-5 col-xl-8 d-flex flex-column justify-content-start align-items-start mb-5"
+            <div class="col-12 col-md-6 col-lg-6 col-xl-8 d-flex flex-column align-items-center justify-content-center  justify-content-lg-start align-items-lg-start mb-3 mb-lg-0"
                 data-aos="fade-up" data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000"
                 data-aos-easing="ease-in-out">
-                <div
-                    class="d-flex flex-column flex-lg-row justify-content-between align-items-start align-items-lg-center">
-                    <div class="content">
+                <div class="d-flex flex-column text-center text-lg-start   align-items-center justify-content-center justify-content-lg-between align-items-lg-start">
                         <h3><?= $fields['title']; ?></h3>
                         <div class="regular">
                             <?= $fields['text']; ?>
                         </div>
-                    </div>
-
                 </div>
             </div>
             <div
-                class=" col-md-6 col-lg-3 offset-lg-1 col-xl-3 offset-xl-1 d-flex align-items-center justify-content-center">
+                class=" col-md-6 col-lg-5 offset-lg-1 col-xl-3 offset-xl-1 d-flex align-items-center justify-content-center">
                 <?php if (!empty($fields['button'])): ?>
                     <a href="<?= esc_url($fields['button']['url']); ?>"
                         target="<?= esc_attr($fields['button']['target']); ?>" class="button custom-button">
