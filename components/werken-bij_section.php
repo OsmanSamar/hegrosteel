d739ec270922b647afb5146e01d7d3e66e902bce
Samar <osmansamar367@gmail.com>
2025-05-27 15:20:17 +0200

    Removed unused div and styled

--- components/werken-bij_section.php
+++ components/werken-bij_section.php
@@ -2,64 +2,53 @@
 $fields = $args['fields'];
 ?>
 <div class="werken-bij_section">
-    <div class="">
-        <div class="row">
-            <div class="col-12 col-md-6 col-lg-5 offset-lg-1  d-flex flex-column justify-content-start align-items-start mb-4 mb-lg-0"
-                data-aos="fade-up" data-aos-offset="100" data-aos-delay="100" data-aos-duration="1000"
-                data-aos-easing="ease-in-out">
-                <div class="d-flex flex-column flex-lg-row  align-items-center ">
-                    <!-- Used Group -->
-                    <div class="shared-content right-bottom-shape">
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-5 offset-lg-1  mb-4 mb-lg-0" data-aos="fade-up" data-aos-offset="100"
+            data-aos-delay="100" data-aos-duration="1000" data-aos-easing="ease-in-out">
+            <!-- Used Group -->
+            <div class="shared-content right-bottom-shape">
+                <a href="<?= esc_url($fields['blok_1']['link']['url']); ?>">
+                    <div class="lead "><?= $fields['blok_1']['sub_title']; ?></div>
+                    <h3 style="font-family: Funnel Display"><?= $fields['blok_1']['title']; ?></h3>
+                    <div class="lead"><?= $fields['blok_1']['text']; ?></div>
 
-                        <a href="<?= esc_url($fields['blok_1']['link']['url']); ?>">
-                            <div class="lead "><?= $fields['blok_1']['sub_title']; ?></div>
-                            <h3 style="font-family: Funnel Display"><?= $fields['blok_1']['title']; ?></h3>
-                            <div class="lead"><?= $fields['blok_1']['text']; ?></div>
-
-                            <img loading="lazy" src="<?= get_template_directory_uri(); ?>/images/white-arrow.svg"
-                                alt="Arrow" class="werk-bij-arrow red-arrow">
-                        </a>
-                    </div>
-                </div>
-
-
+                    <img loading="lazy" src="<?= get_template_directory_uri(); ?>/images/white-arrow.svg" alt="Arrow"
+                        class="werk-bij-arrow red-arrow">
+                </a>
             </div>
-            <div class="col-lg-5 col-md-6 mt-auto" data-aos="fade-up" data-aos-offset="100" data-aos-delay="250" data-aos-duration="1000"
-                data-aos-easing="ease-in-out">
-                <div class="shared-content  left-bottom-shape  position-relative">
-                    <a href="<?= esc_url($fields['blok_2']['link']['url']); ?>">
-                        <div class="lead  "><?= $fields['blok_2']['sub_title']; ?></div>
-                        <h3 style="font-family: Funnel Display"><?= $fields['blok_2']['title']; ?></h3>
-                        <div class="lead"><?= $fields['blok_2']['text']; ?></div>
+        </div>
+
+        <div class="col-lg-5 col-md-6 mt-auto" data-aos="fade-up" data-aos-offset="100" data-aos-delay="250"
+            data-aos-duration="1000" data-aos-easing="ease-in-out">
+            <div class="shared-content  left-bottom-shape  position-relative">
+                <a href="<?= esc_url($fields['blok_2']['link']['url']); ?>">
+                    <div class="lead  "><?= $fields['blok_2']['sub_title']; ?></div>
+                    <h3 style="font-family: Funnel Display"><?= $fields['blok_2']['title']; ?></h3>
+                    <div class="lead"><?= $fields['blok_2']['text']; ?></div>
+                    <img loading="lazy" src="<?= get_template_directory_uri(); ?>/images/white-arrow.svg" alt="Arrow"
+                        class="werk-bij-arrow white-arrow">
+                </a>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-5 offset-lg-6 col-xl-4 col-md-6 offset-md-6 mt-4" data-aos="fade-up"
+                data-aos-offset="100" data-aos-delay="300" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                <div class="shared-content  left-top-shape">
+                    <a href="<?= esc_url($fields['blok_3']['link']['url']); ?>">
+                        <div class="lead back-text"><?= $fields['blok_3']['sub_title']; ?></div>
+                        <h3 class="back-text" style="font-family: Funnel Display"><?= $fields['blok_3']['title']; ?>
+                        </h3>
+                        <div class="lead"><?= $fields['blok_3']['text']; ?></div>
                         <img loading="lazy" src="<?= get_template_directory_uri(); ?>/images/white-arrow.svg"
-                            alt="Arrow" class="werk-bij-arrow white-arrow">
+                            alt="Arrow" class="werk-bij-arrow black-arrow">
                     </a>
                 </div>
             </div>
-
-
-
-
-
-
-
-            <div class="row">
-                <div class="col-lg-5 offset-lg-6 col-xl-4 col-md-6 offset-md-6 mt-4" data-aos="fade-up" data-aos-offset="100" data-aos-delay="300" data-aos-duration="1000"
-                data-aos-easing="ease-in-out">
-                    <div class="shared-content  left-top-shape">
-                        <a href="<?= esc_url($fields['blok_3']['link']['url']); ?>">
-                            <div class="lead back-text"><?= $fields['blok_3']['sub_title']; ?></div>
-                            <h3 class="back-text" style="font-family: Funnel Display"><?= $fields['blok_3']['title']; ?>
-                            </h3>
-                            <div class="lead"><?= $fields['blok_3']['text']; ?></div>
-                            <img loading="lazy" src="<?= get_template_directory_uri(); ?>/images/white-arrow.svg"
-                                alt="Arrow" class="werk-bij-arrow black-arrow">
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-
         </div>
+
+
     </div>
+
 </div>
